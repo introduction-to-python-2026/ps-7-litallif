755744232bba426ba460e038d259b803105ef07b
litallif <liflital452@gmail.com>
2025-12-23 20:51:09 +0200

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import fetch_openml
+import pandas as pd
+data=fetch_openml(name='wine', version=1, as_frame=True)
+print(data.DESCR)
+df = data.frame
+df.sample(5) 
+df.describe()
+df.dtypes 
+features = list(data.frame.columns)
+print("Available features:", features)
+features = df.columns
+selected_features = 'class','Alcohol','Malic_acid','Ash','Alcalinity_of_ash'
+fig, axs  = plt.subplots(1, len(selected_features), figsize = (20,3))
+for ax, f in zip(axs, selected_features):
+    ax.hist(df[f], bins=5, color='skyblue', edgecolor='black')
+    ax.set_xlabel(f)
+reference_feature = selected_features[1]
+y = df[reference_feature]
+fig, axs  = plt.subplots(1, len(selected_features), figsize = (20,3))
+for ax, f in zip(axs, selected_features):
+  ax.scatter(df[f], y)
+  ax.set_xlabel(f)
+plt.show()
+reference_feature = selected_features[0]  
+comparison_feature = selected_features[1]  
+plt.figure(figsize=(8, 6))
+plt.scatter(df[reference_feature], df[comparison_feature], alpha=0.6)
+plt.xlabel(reference_feature)
+plt.ylabel(comparison_feature)
+plt.savefig('correlation_plot.png')
+plt.show()
